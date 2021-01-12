72f97b4951d694d76ead0eaa71e936f0ba0d5a52
Sophia Mazurek <sophiamazurek@Sophias-MBP-2.lan>
2021-01-11 22:01:42 -0600

    updated question array

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,102 @@
-var startbtn = document.getElementById("startbtn");
+//var startbtn = document.getElementById("startbtn");
 var quesitonareaDiv = document.getElementById("questionarea");
-var currentQuestionIndex = 0;
+var index = 0;
 var truebtn = document.querySelector("#truebtn");
 
-var startQuiz = function () {
-    var question =document.getElementById("questionArea");
-    question.innerHTML = questions[currentQuestionIndex].q;
-    timer = setInterval(clock, 6000)
+function startQuiz() {
+   
+    timer = setInterval(clock, 1000);
+    displayCard();
+
+  
+
 }
+
+function displayCard(){
+    //when the game ends wrt the array
+    //questions.length-1
+    //index
+    //when to go to the next card
+    console.log("index "+ index);
+    if(index< questions.length){
+
+  
+    //empty out the btns
+    document.querySelector("#choices").textContent="";
+    var question =document.getElementById("questionArea");
+    question.innerHTML = questions[index].q;
+      // <button id="choicebtn" class="buttons choicebtn" value="false">FALSE</button>
+      var btn = document.createElement("button");
+      //<button></button>
+      btn.setAttribute("id","choicebtn");
+        //<button id="choicebtn"></button>
+      btn.setAttribute("class","choicebtn buttons");
+      btn.setAttribute("value", "f");
+         //<button id="choicebtn" value="false"></button>
+      btn.textContent="FALSE";
+      //<button id="choicebtn" value="false">FALSE</button>
+      btn.onclick = checkAns;
+      //stick to the page ( #choices)
+      document.querySelector("#choices").appendChild(btn)
+  
+      var btn2 = document.createElement("button");
+      //<button></button>
+      btn2.setAttribute("id","choicebtn");
+        //<button id="choicebtn"></button>
+      btn2.setAttribute("class","choicebtn buttons");
+      btn2.setAttribute("value", "t");
+         //<button id="choicebtn" value="true"></button>
+      btn2.textContent="TRUE";
+      //<button id="choicebtn" value="false">TRUE</button>
+      btn2.onclick = checkAns;
+      //stick to the page ( #choices)
+      document.querySelector("#choices").appendChild(btn2)
+    }
+    //when you finish the game
+ 
+    if(index >questions.length-1){
+        gameOver();
+    }
+    //run out of time (do later if cond)
+
+}
+
+function checkAns(){
+    //get user data
+    console.log(this.value)
+    //get actual ans
+    console.log(questions[index].a)
+    //compare
+    //cases:
+    //right
+        //userinput == actual ans
+            //what do we do? display "correct" ("#answercorrect")
+            if(this.value == questions[index].a){
+                document.querySelector("#answercorrect").textContent = "Correct";
+            }
+    //wrong
+        //userinput != act ans
+            //display "wrong" ("#answercorrect")
+            //subtract time (do later)
+            if(this.value != questions[index].a){
+                document.querySelector("#answercorrect").textContent = "Wrong";
+            }
+    
+    //update index
+    index++;
+    displayCard();
+
+
+}
+
+function gameOver(){
+    alert("game over");
+    //display enter your name area
+    //get userinput and their score (time) and store to local storage
+    //trick is to store in an array of object (when you store into local storage it needs to be a string (stringify()))
+}
+
+document.querySelector(".startbtn").onclick = startQuiz;
 
 
 // Refrence for the questions (taken from quizlet) https://quizlet.com/117039332/javascript-quiz-flash-cards/ and https://quizlet.com/393399799/javascript-exam-i-flash-cards/
@@ -42,37 +131,37 @@ function clock() {
 //Display the start of the game with a score of zero
 var score = 0;
 
-let buttonsArray = document.querySelector("#truebtn");
-trueBtn.addEventListener("click", function(){ console.log("you clicked true") } )
-    var question = document.getElementById("questionArea");
-    var answer = elm.id;
-    console.log(answer);
-    console.log(questions[currentQuestionIndex]);
+//let buttonsArray = document.querySelector("#truebtn");
+// document.querySelector(".choicebtn").addEventListener("click", function(){ 
+//     console.log(this.value)
+//     // var question = document.getElementById("questionArea")
+//     // var answer = elem.id;
+//     // console.log(answer);
+//     // console.log(questions[currentQuestionIndex]);
 
-    if (
-        (answer === "true" && questions[currentQuestionIndex].a === 't') ||
-        (answer === "false" && questions[currentQuestionIndex].a === 'f')
-    ) {
+//     // if (
+//     //     (answer === "true" && questions[currentQuestionIndex].a === 't') ||
+//     //     (answer === "false" && questions[currentQuestionIndex].a === 'f')
+//     // ) {
 
-        document.getElementById("answercorrect").innerHTML = "Correct!";
-        currentQuestionIndex++;
-    } else {
-        time -= 15;
+//     //     document.getElementById("answercorrect").innerHTML = "Correct!";
+//     //     currentQuestionIndex++;
+//     // } else {
+//     //     time -= 15;
 
-        document.getElementById("answercorrect").innerHTML = "Wrong!";
-        currentQuestionIndex++;
-    };
-    console.log("question: ", currentQuestionIndex)
+//     //     document.getElementById("answercorrect").innerHTML = "Wrong!";
+//     //     currentQuestionIndex++;
+//     // };
+//     // console.log("question: ", currentQuestionIndex)
 
-    if (currentQuestionIndex < questions.length) {
+//     // if (currentQuestionIndex < questions.length) {
 
-        question.innerHTML = questions[currentQuestionIndex].q;
-    } else {
-        quizEnd()
-    }
+//     //     question.innerHTML = questions[currentQuestionIndex].q;
+//     // } else {
+//     //     quizEnd()
+//     // }
     
-;
-
+// });
 function quizEnd() {
     console.log("quizend:", time)
     quesitonareaDiv.innerHTML = ""
@@ -89,7 +178,7 @@ function getScore(forwho) {
     return localStorage.getItem(forwho)
 }
 
-//Refrence: alis github add in 
+//Refrence: https://github.com/alirueter/code-quiz 
 document.getElementById('highscoreInput').innerHTML = ` 
     Enter your name:
     <input type="text" id="initialInput"></input>
@@ -97,5 +186,5 @@ document.getElementById('highscoreInput').innerHTML = `
     `
 var initialInputEl = document.getElementById("initialInput");
 
-startbtn.onclick = startQuiz;
+
 getScore();
